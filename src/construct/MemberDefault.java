ef9ab6c5680e0c91b69fe47db99cb288b0d89632
rhos0129 <rhos0129@gmail.com>
2024-08-02 14:01:27 +0900

    S04 기본 생성자

---
+++ src/construct/MemberDefault.java
@@ -0,0 +1,9 @@
+package construct;
+
+public class MemberDefault {
+
+    String name;
+
+    // 직접 정의된 생성자가 없으므로 자바가 자동으로 기본 생성자를 만들어 준다.
+
+}
---
+++ src/construct/MemberDefaultMain.java
@@ -0,0 +1,12 @@
+package construct;
+
+public class MemberDefaultMain {
+
+    public static void main(String[] args) {
+
+        // 기본 생성자 호출
+        MemberDefault memberDefault = new MemberDefault();
+
+    }
+
+}
