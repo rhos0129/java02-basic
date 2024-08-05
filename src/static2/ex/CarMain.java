2d9658c6c2cc60f6766bf46d8e50546b57533ab3
rhos0129 <rhos0129@gmail.com>
2024-08-05 16:39:55 +0900

    S07 문제와 풀이

---
+++ src/static2/ex/CarMain.java
@@ -0,0 +1,17 @@
+package static2.ex;
+
+public class CarMain {
+
+    public static void main(String[] args) {
+
+        // 다음 코드를 참고해서 생성한 차량 수를 출력하는 프로그램 위한 Car 클래스를 작성하자.
+
+        Car car1 = new Car("K3");
+        Car car2 = new Car("G80");
+        Car car3 = new Car("Model Y");
+
+        Car.showTotalCars(); // 구매한 차량 수를 출력하는 static 메서드
+
+    }
+
+}
