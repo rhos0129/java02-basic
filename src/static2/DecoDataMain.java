29bf3a0d9b951580eddbbd3d596396088ae911bb
rhos0129 <rhos0129@gmail.com>
2024-08-05 16:35:58 +0900

    S07 static 메서드3

--- src/static2/DecoDataMain.java
+++ src/static2/DecoDataMain.java
@@ -1,5 +1,8 @@
 package static2;
 
+//import static static2.DecoData.staticCall;
+//import static static2.DecoData.*;
+
 public class DecoDataMain {
 
     public static void main(String[] args) {
@@ -14,6 +17,13 @@ public class DecoDataMain {
         System.out.println("3. 인스턴스 호출2");
         DecoData data2 = new DecoData();
         data2.instanceCall(); // staticValue = 3
+
+        // 인스턴스를 통한 접근 -> 추천X (인스턴스 변수에 접근하는 것 처럼 오해할 수 있다.)
+        DecoData data3 = new DecoData();
+        data3.staticCall();
+
+        // 클래스를 통한 접근 -> 추천O (클래스를 통해서 접근하는 것이 더 명확하다.)
+        DecoData.staticCall();
 
     }
 }
