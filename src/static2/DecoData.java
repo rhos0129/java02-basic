0c79f53e27bd9ed32b3f828c80f6ed488b43e518
rhos0129 <rhos0129@gmail.com>
2024-08-05 16:28:43 +0900

    S07 static 메서드2

---
+++ src/static2/DecoData.java
@@ -0,0 +1,33 @@
+package static2;
+
+public class DecoData {
+
+    private int instanceValue;
+    private static int staticValue;
+
+    public static void staticCall() {
+//        instanceValue++; // 인스턴스 변수 접근 불가, 컴파일 오류
+//        instanceMethod(); // 인스턴스 메서드 접근 불가, 컴파일 오류
+
+        staticValue++; // 정적 변수 접근
+        staticMethod(); // 정적 메서드 접근
+    }
+
+    public void instanceCall() {
+        instanceValue++; // 인스턴스 변수 접근
+        instanceMethod(); // 인스턴스 메서드 접근
+
+        staticValue++; // 정적 변수 접근
+        staticMethod(); // 정적 메서드 접근
+    }
+
+
+    private void instanceMethod() {
+        System.out.println("instanceValue = " + instanceValue);
+    }
+
+    private static void staticMethod() {
+        System.out.println("staticValue = " + staticValue);
+    }
+
+}
