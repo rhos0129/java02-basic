a2bfcdcf36d48edd28c860d12cc1963ac09996b3
rhos0129 <rhos0129@gmail.com>
2024-08-06 15:23:08 +0900

    S08 final 변수와 상수2

---
+++ src/final1/ConstantMain1.java
@@ -0,0 +1,25 @@
+package final1;
+
+public class ConstantMain1 {
+
+    public static void main(String[] args) {
+
+        // 상수를 사용하지 않는 경우
+        System.out.println("프로그램 최대 참여자 수 " + 1000);
+        int currentUserCount = 999;
+        process(currentUserCount++);
+        process(currentUserCount++);
+        process(currentUserCount++);
+
+    }
+
+    private static void process(int currentUserCount) {
+        System.out.println("참여자 수:" + currentUserCount);
+        if (currentUserCount > 1000) {
+            System.out.println("대기자로 등록합니다.");
+        } else {
+            System.out.println("게임에 참여합니다.");
+        }
+    }
+
+}
