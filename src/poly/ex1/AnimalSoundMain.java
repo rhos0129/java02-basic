5ee3ebd8eba92b890167f78e8e3677c1301d3448
rhos0129 <rhos0129@gmail.com>
2024-08-08 19:34:58 +0900

    S11 다형성 활용1

---
+++ src/poly/ex1/AnimalSoundMain.java
@@ -0,0 +1,28 @@
+package poly.ex1;
+
+public class AnimalSoundMain {
+
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Cow cow = new Cow();
+
+        System.out.println("동물 소리 테스트 시작");
+        dog.sound();
+        System.out.println("동물 소리 테스트 종료");
+
+        System.out.println("동물 소리 테스트 시작");
+        cat.sound();
+        System.out.println("동물 소리 테스트 종료");
+
+        System.out.println("동물 소리 테스트 시작");
+        cow.sound();
+        System.out.println("동물 소리 테스트 종료");
+
+        // 모두 타입이 다르므로
+        // 메서드로도, 배열이나 for문으로도 중복코드를 제거할 수 없다.
+
+    }
+
+}
---
+++ src/poly/ex1/Cow.java
@@ -0,0 +1,9 @@
+package poly.ex1;
+
+public class Cow {
+
+    public void sound() {
+        System.out.println("음매");
+    }
+
+}
