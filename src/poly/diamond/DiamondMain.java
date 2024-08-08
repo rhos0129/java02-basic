af5c3ad20a59cc569ea1e6114f005ea5306999c0
rhos0129 <rhos0129@gmail.com>
2024-08-08 20:59:33 +0900

    S11 인터페이스 - 다중 구현

---
+++ src/poly/diamond/DiamondMain.java
@@ -0,0 +1,20 @@
+package poly.diamond;
+
+public class DiamondMain {
+
+    public static void main(String[] args) {
+
+        InterfaceA a = new Child();
+        a.methodA();
+        a.methodCommon();
+
+        InterfaceB b = new Child();
+        b.methodB();
+        b.methodCommon();
+
+        Child c = new Child();
+        c.methodCommon();
+
+    }
+
+}
